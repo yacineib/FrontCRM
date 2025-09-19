5cbb2022a414b98f99603357ad5cf9fd59055196
Yassine Ibrahimi <yacine.ibrahimi00@gmail.com>
2025-09-19 09:02:40 +0100

    first commit

--- DouaneCRM.Presentation/obj/Debug/net8.0/DouaneCRM.Presentation.AssemblyInfo.cs
+++ DouaneCRM.Presentation/obj/Debug/net8.0/DouaneCRM.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DouaneCRM.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+241d8f22644dd13e6a320764b649e94aac1b9d17")]
 [assembly: System.Reflection.AssemblyProductAttribute("DouaneCRM.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DouaneCRM.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
